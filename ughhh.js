e12fcbd983d00483a563656fe6fc8f17b3bf83cd
pagkukudigo <saralyns@uw.edu>
2017-10-22 01:09:01 -0700

    added about page

--- ughhh.js
+++ ughhh.js
@@ -1,13 +1,16 @@
 // Main Javascript file (ughhhhhhh) (i hate javascript)
 
+$(function () {
 
-var config = {
-    apiKey: "AIzaSyARpu_8IyhaHqBysC04hzJO497s7tkXNSc",
-    authDomain: "dubhacks2017-4df7e.firebaseapp.com",
-    databaseURL: "https://dubhacks2017-4df7e.firebaseio.com",
-    projectId: "dubhacks2017-4df7e",
-    storageBucket: "",
-    messagingSenderId: "790915466653"
-};
+   var config = {
+	  apiKey: "AIzaSyARpu_8IyhaHqBysC04hzJO497s7tkXNSc",
+	  authDomain: "dubhacks2017-4df7e.firebaseapp.com",
+	  databaseURL: "https://dubhacks2017-4df7e.firebaseio.com",
+	  projectId: "dubhacks2017-4df7e",
+	  storageBucket: "",
+	  messagingSenderId: "790915466653"
+	  };
 
-firebase.initializeApp(config);
+	  firebase.initializeApp(config);
+	  
+});
