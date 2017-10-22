64ae16b5731b68b1b16d04419b7b4eaeee04d387
pagkukudigo <saralyns@uw.edu>
2017-10-22 01:23:48 -0700

    uh

--- ughhh.js
+++ ughhh.js
@@ -1,16 +1,2 @@
 // Main Javascript file (ughhhhhhh) (i hate javascript)
 
-$(function () {
-
-   var config = {
-	  apiKey: "AIzaSyARpu_8IyhaHqBysC04hzJO497s7tkXNSc",
-	  authDomain: "dubhacks2017-4df7e.firebaseapp.com",
-	  databaseURL: "https://dubhacks2017-4df7e.firebaseio.com",
-	  projectId: "dubhacks2017-4df7e",
-	  storageBucket: "",
-	  messagingSenderId: "790915466653"
-	  };
-
-	  firebase.initializeApp(config);
-	  
-});
