92c73664c8ce343d3cc32f6755ebc01354e634e6
pagkukudigo <saralyns@uw.edu>
2017-10-22 02:22:08 -0700

    figured out jquery!!!!

--- ughhh.js
+++ ughhh.js
@@ -1,2 +1,14 @@
 // Main Javascript file (ughhhhhhh) (i hate javascript)
 
+$(function(){
+
+ // jQuery methods go here...
+
+ /*$("button").click(function() {
+  $("button").after("<form id='helllll yeah'>does this thing work????</div>");
+ });*/
+ 
+ 
+ 
+
+});
