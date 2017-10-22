5079c911e34a833b8212990a6f66b204750a6bc6
Madisen Arurang <madireyn@uw.edu>
2017-10-22 02:07:21 -0700

    whoops

--- test/map.js
+++
@@ -1,8 +0,0 @@
- function GetMap()
-	{
-		var map = new Microsoft.Maps.Map('#map', {
-			credentials: 'Av3xH_roBTVysTGJ4wiUFWTJrrcoImteHpoWopVXubmrXEn59pILgkO7jrUSaX5j'
-		});
-
-		//Add your post map load code here.
-	}
