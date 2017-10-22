e86f08402b0ad4da7ab249d89a7f1d9ef494fee7
ryanfok <ryanfok@cs.washington.edu>
2017-10-22 03:56:11 -0700

    Routing
    input for to and from with route finding

--- map.js
+++ map.js
@@ -1,8 +1,18 @@
  function GetMap()
 	{
 		var map = new Microsoft.Maps.Map('#map', {
-			credentials: 'ArH86Tt6a9tC7LMFqNXruTOPXaeWxQFuctvl7O3JKoAAU3KVM5bSd-wu4JN66Cem'
+			credentials: 'ArH86Tt6a9tC7LMFqNXruTOPXaeWxQFuctvl7O3JKoAAU3KVM5bSd-wu4JN66Cem',
+			showMapTypeSelector: false
 		});
 
-		//Add your post map load code here.
+		Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
+	    	var dirManager = new Microsoft.Maps.Directions.DirectionsManager(map);
+    		dirManager.setRenderOptions({ itineraryContainer: document.getElementById('printoutPanel') });
+    		dirManager.showInputPanel('directionsInputContainer');
+		});
+
+		Microsoft.Maps.loadModule('Microsoft.Maps.Traffic', function () {
+		    var manager = new Microsoft.Maps.Traffic.TrafficManager(map);
+		    manager.show();
+		});
 	}
