e8a407c02473e1e37a529ddb620bdb84c553f2dc
pagkukudigo <saralyns@uw.edu>
2017-10-22 04:00:53 -0700

    figured out form!!!

--- ughhh.js
+++ ughhh.js
@@ -2,13 +2,36 @@
 
 $(function(){
 
+ var config = {
+  apiKey: "AIzaSyARpu_8IyhaHqBysC04hzJO497s7tkXNSc",
+  authDomain: "dubhacks2017-4df7e.firebaseapp.com",
+  databaseURL: "https://dubhacks2017-4df7e.firebaseio.com",
+  projectId: "dubhacks2017-4df7e",
+  storageBucket: "dubhacks2017-4df7e.appspot.com",
+  messagingSenderId: "790915466653"
+ };
+ firebase.initializeApp(config);
  // jQuery methods go here...
 
- /*$("button").click(function() {
-  $("button").after("<form id='helllll yeah'>does this thing work????</div>");
- });*/
- 
- 
- 
+ var info = firebase.database().ref('info');
+
+
+ $("#sub").click(function() {
+  event.preventDefault();
+  alert("hey does this work");
+  
+  var childName = $("#child").val();
+  var emergencyNo = $("#emerno").val();
+  
+  info.push({
+   child: childName,
+   phone: emergencyNo
+  });
+  
+  $(this).after("<div>your child's name is " + childName + " and the emergency number to contact is " + emergencyNo);
+  
+  $(this).reset();
+ });
+
 
 });
